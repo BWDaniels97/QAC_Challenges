3f20a4eed9dba0f4c4427eebed091260a478bb7a
Bradley-Daniels <bwdaniels97@gmail.com>
2020-07-16 17:35:00 +0100

    Challenge of the day 1

---
+++ Challenge_1.py
@@ -0,0 +1,17 @@
+
+''' 
+CHALLENGE 1
+
+- initialises a list which contains the FIRST NAME of all 21 people in this cohort
+- using the .append() function add me to the list
+- print the 5th person in the list (this is a trick question, what is special about the way Python indexes?)
+- print the number of Chris in the cohort 
+
+'''
+
+
+Names = ['Brad','Chris','John','Jason','Arsalan','Domenico','Joshua','Ryan','Javas','Sithembiso','Jacob','Clifford','Wasim','Mohamed','Tobias', 'Chris','Samuel','Edmund','Amanda','Steven','Diarmuid']
+Names.append('Luke')
+count = Names.count('Chris')
+print(Names[4])
+print(count)
